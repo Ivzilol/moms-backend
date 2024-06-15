53314168adb8b25df2a84dcf3e387785301433c7
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-06-15 15:46:57 +0300

    user-register validations

---
+++ user-management-microservice/user-command-service/src/test/java/bg/mck/usercommandservice/UserIntegrationTests/UserControllerTest.java
@@ -0,0 +1,135 @@
+package bg.mck.usercommandservice.UserIntegrationTests;
+
+
+import bg.mck.usercommandservice.application.controller.UserCommandController;
+import bg.mck.usercommandservice.application.dto.UserRegisterDTO;
+import bg.mck.usercommandservice.application.entity.Authority;
+import bg.mck.usercommandservice.application.service.UserCommandService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest {
+
+    @LocalServerPort
+    private int port;
+    private String baseUrl = "http://localhost";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        baseUrl = baseUrl.concat(":").concat(port + "").concat("/users");
+
+        UserRegisterDTO userRegisterDTO = new UserRegisterDTO();
+        userRegisterDTO.setEmail("ivo_ali@abv.bg");
+        userRegisterDTO.setPassword("asdasd");
+        userRegisterDTO.setConfirmPassword("asdasd");
+        userRegisterDTO.setFirstName("Gosho");
+        userRegisterDTO.setLastName("Georgiev");
+        userRegisterDTO.setPhoneNumber("0888776655");
+        userRegisterDTO.setRole("user");
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRegisterDTO)));
+    }
+
+
+    @Test
+    public void userRegister_Successful_Test() throws Exception {
+        UserRegisterDTO userRegisterDTO = new UserRegisterDTO();
+        userRegisterDTO.setEmail("ivo_ali3@abv.bg");
+        userRegisterDTO.setPassword("asdasd");
+        userRegisterDTO.setConfirmPassword("asdasd");
+        userRegisterDTO.setFirstName("Gosho");
+        userRegisterDTO.setLastName("Georgiev");
+        userRegisterDTO.setPhoneNumber("0888776655");
+        userRegisterDTO.setRole("user");
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRegisterDTO)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void userRegister_EmailExist_Test() throws Exception {
+        UserRegisterDTO userRegisterDTO = new UserRegisterDTO();
+        userRegisterDTO.setEmail("ivo_ali@abv.bg");
+        userRegisterDTO.setPassword("asdasd");
+        userRegisterDTO.setConfirmPassword("asdasd");
+        userRegisterDTO.setFirstName("Gosho");
+        userRegisterDTO.setLastName("Georgiev");
+        userRegisterDTO.setPhoneNumber("0888776655");
+        userRegisterDTO.setRole("user");
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRegisterDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.emailError")
+                        .value("Email already exist"))
+                .andReturn();
+    }
+
+    @Test
+    public void userRegister_PasswordIsToShort_Test() throws Exception {
+        UserRegisterDTO userRegisterDTO = new UserRegisterDTO();
+        userRegisterDTO.setEmail("ivo_ali3@abv.bg");
+        userRegisterDTO.setPassword("asd");
+        userRegisterDTO.setConfirmPassword("asd");
+        userRegisterDTO.setFirstName("Gosho");
+        userRegisterDTO.setLastName("Georgiev");
+        userRegisterDTO.setPhoneNumber("0888776655");
+        userRegisterDTO.setRole("user");
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRegisterDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.passwordError")
+                        .value("The password must contain a minimum of 6 characters"))
+                .andReturn();
+    }
+
+    @Test
+    public void userRegister_NameIsEmpty_Test() throws Exception {
+        UserRegisterDTO userRegisterDTO = new UserRegisterDTO();
+        userRegisterDTO.setEmail("ivo_ali3@abv.bg");
+        userRegisterDTO.setPassword("asdasd");
+        userRegisterDTO.setConfirmPassword("asdasd");
+        userRegisterDTO.setFirstName("");
+        userRegisterDTO.setLastName("Georgiev");
+        userRegisterDTO.setPhoneNumber("0888776655");
+        userRegisterDTO.setRole("user");
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRegisterDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstNameError")
+                        .value("First Name cannot be empty"))
+                .andReturn();
+    }
+}
