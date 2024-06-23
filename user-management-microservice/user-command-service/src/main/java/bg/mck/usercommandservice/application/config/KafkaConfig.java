6b50db2a60d7497c6ae066b9e93ff5cd121d3493
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-06-23 10:55:20 +0300

    Refactor User Exceptions upon registrations

--- user-management-microservice/user-command-service/src/main/java/bg/mck/usercommandservice/application/config/KafkaConfig.java
+++ user-management-microservice/user-command-service/src/main/java/bg/mck/usercommandservice/application/config/KafkaConfig.java
@@ -1,24 +1,24 @@
-package bg.mck.usercommandservice.application.config;
-
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
-
-@Configuration
-public class KafkaConfig {
-
-    private final String topicName;
-
-    public KafkaConfig(@Value("${KAFKA.USER.TOPIC.NAME}") String topicName) {
-        this.topicName = topicName;
-    }
-
-    @Bean
-    public NewTopic userTopic() {
-        return TopicBuilder
-                .name(topicName)
-                .build();
-    }
-}
+//package bg.mck.usercommandservice.application.config;
+//
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.TopicBuilder;
+//
+//@Configuration
+//public class KafkaConfig {
+//
+//    private final String topicName;
+//
+//    public KafkaConfig(@Value("${KAFKA.USER.TOPIC.NAME}") String topicName) {
+//        this.topicName = topicName;
+//    }
+//
+//    @Bean
+//    public NewTopic userTopic() {
+//        return TopicBuilder
+//                .name(topicName)
+//                .build();
+//    }
+//}
--- user-management-microservice/user-command-service/src/test/java/bg/mck/usercommandservice/UserIntegrationTests/UserControllerTest.java
+++ user-management-microservice/user-command-service/src/test/java/bg/mck/usercommandservice/UserIntegrationTests/UserControllerTest.java
@@ -10,8 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
