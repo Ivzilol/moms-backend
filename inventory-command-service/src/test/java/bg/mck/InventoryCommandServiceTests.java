3eebc834ff3744c493cf0c083e8b734f5adcbdc1
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-06-27 09:41:37 +0300

    Add Material in DB

--- inventory-command-service/src/test/java/bg/mck/InventoryCommandServiceTests.java
+++ inventory-command-service/src/test/java/bg/mck/InventoryCommandServiceTests.java
@@ -1,13 +1,13 @@
-package bg.mck.usercommandservice;
+package bg.mck;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class   UserRegisterServiceApplicationTests {
+public class InventoryCommandServiceTests {
 
     @Test
     void contextLoads() {
-    }
 
+    }
 }
