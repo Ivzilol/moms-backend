2a623e3cd68e6652e904248f6ca02573552e2f9f
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-06-27 09:17:13 +0300

    Add Material in DB

--- inventory-command-service/src/main/java/bg/mck/controller/InventoryController.java
+++ inventory-command-service/src/main/java/bg/mck/controller/InventoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/${APPLICATION_VERSION}/inventory")
+@RequestMapping("/${APPLICATION_VERSION}/inventory/command")
 public class InventoryController {
 
     private final InventoryService inventoryService;
