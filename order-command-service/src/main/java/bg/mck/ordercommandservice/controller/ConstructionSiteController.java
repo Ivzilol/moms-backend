c46242a9e8c09f69b36a57cc2c90e763e185dadc
Mihail Slavov <mihail_slavov@icloud.com>
2024-07-07 12:40:20 +0300

    add Swagger to Create ConstructionSite

--- order-command-service/src/main/java/bg/mck/ordercommandservice/controller/ConstructionSiteController.java
+++ order-command-service/src/main/java/bg/mck/ordercommandservice/controller/ConstructionSiteController.java
@@ -1,7 +1,13 @@
 package bg.mck.ordercommandservice.controller;
 
 import bg.mck.ordercommandservice.dto.ConstructionSiteDTO;
+import bg.mck.ordercommandservice.exception.ConstructionSiteAlreadyExists;
 import bg.mck.ordercommandservice.service.ConstructionSiteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +24,14 @@ public class ConstructionSiteController {
         this.constructionSiteService = constructionSiteService;
     }
 
+
+    @Operation(summary = "Create construction site")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Order created successfully"),
+            @ApiResponse(responseCode = "400", description = "Incorrect data"),
+            @ApiResponse(responseCode = "409", description = "Construction site already exists")
+    }
+    )
     @PostMapping("/create-construction-site")
     public ResponseEntity<ConstructionSiteDTO> createConstructionSite(@RequestBody @Valid ConstructionSiteDTO constructionSiteDTO) {
         return ResponseEntity.ok(constructionSiteService.createConstructionSite(constructionSiteDTO));
