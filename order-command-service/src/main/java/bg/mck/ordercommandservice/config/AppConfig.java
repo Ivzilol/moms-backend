8fe342ffa2c0e600522ee51f619b84b7c7d6f3f6
Mihail Slavov <mihail_slavov@icloud.com>
2024-07-11 07:21:17 +0300

    add @LoadBalanced, so we can call services by name

--- order-command-service/src/main/java/bg/mck/ordercommandservice/config/AppConfig.java
+++ order-command-service/src/main/java/bg/mck/ordercommandservice/config/AppConfig.java
@@ -1,6 +1,7 @@
 package bg.mck.ordercommandservice.config;
 
 import jakarta.transaction.Transactional;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
