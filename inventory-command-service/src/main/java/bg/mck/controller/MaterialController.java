5db04dc48feaacf95f6fe9df55f509c8efda1a3e
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-07-11 12:25:16 +0300

    Inventory Service - validation material

--- inventory-command-service/src/main/java/bg/mck/controller/MaterialController.java
+++ inventory-command-service/src/main/java/bg/mck/controller/MaterialController.java
@@ -22,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static bg.mck.errors.ErrorsCreateMaterial.MATERIAL_EXIST;
+
 @RestController
 @RequestMapping("/${APPLICATION_VERSION}/inventory/command")
 public class MaterialController {
@@ -47,17 +49,24 @@ public class MaterialController {
     public ResponseEntity<?> createMaterial(@RequestBody @Valid CreateMaterialDTO createMaterialDTO,
                                             BindingResult result) {
         ResponseEntity<ErrorCreateMaterialDTO> errorCreateMaterialDTO =
-                errorRegistrationMaterial(result);
+                errorRegistrationMaterial(result, createMaterialDTO);
         if (errorCreateMaterialDTO != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(errorCreateMaterialDTO.getBody());
         }
+
         this.inventoryService.createMaterial(createMaterialDTO);
         return ResponseEntity.ok().build();
     }
 
-    private ResponseEntity<ErrorCreateMaterialDTO> errorRegistrationMaterial(BindingResult result) {
+    private ResponseEntity<ErrorCreateMaterialDTO> errorRegistrationMaterial(
+            BindingResult result,
+            CreateMaterialDTO createMaterialDTO) {
         ErrorCreateMaterialDTO errorCreateMaterialDTO = new ErrorCreateMaterialDTO();
+        boolean checkMaterialName = this.inventoryService.checkMaterialName(createMaterialDTO);
+        if (checkMaterialName) {
+            errorCreateMaterialDTO.setMaterialAlreadyExist(MATERIAL_EXIST);
+        }
         if (result.hasErrors()) {
             List<String> errors = result.getAllErrors().stream()
                     .map(DefaultMessageSourceResolvable::getDefaultMessage)
