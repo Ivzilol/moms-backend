6a926a21404a8bc9968be91a8e93f023796bb0c0
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-07-13 13:56:32 +0300

    implement search bay name

---
+++ inventory-query-service/src/main/java/bg/mck/service/MaterialSearchService.java
@@ -0,0 +1,15 @@
+package bg.mck.service;
+
+import bg.mck.dto.MaterialDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MaterialSearchService {
+
+
+    public List<MaterialDTO> findMaterialByCategoryAndName(String category, String materialName) {
+        return null;
+    }
+}
