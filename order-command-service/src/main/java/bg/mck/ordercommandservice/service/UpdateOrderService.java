b0ca7d86b8e4932c0bd72999d201850e064b3025
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-07-21 16:50:38 +0300

    implement update order

--- order-command-service/src/main/java/bg/mck/ordercommandservice/service/UpdateOrderService.java
+++ order-command-service/src/main/java/bg/mck/ordercommandservice/service/UpdateOrderService.java
@@ -5,14 +5,17 @@ import bg.mck.ordercommandservice.dto.UpdateOrderDTO;
 import bg.mck.ordercommandservice.entity.FastenerEntity;
 import bg.mck.ordercommandservice.entity.GalvanisedSheetEntity;
 import bg.mck.ordercommandservice.entity.InsulationEntity;
+import bg.mck.ordercommandservice.entity.MetalEntity;
 import bg.mck.ordercommandservice.entity.enums.MaterialType;
 import bg.mck.ordercommandservice.event.OrderEventType;
 import bg.mck.ordercommandservice.mapper.FastenerMapper;
 import bg.mck.ordercommandservice.mapper.GalvanisedSheetMapper;
 import bg.mck.ordercommandservice.mapper.InsulationMapper;
+import bg.mck.ordercommandservice.mapper.MetalMapper;
 import bg.mck.ordercommandservice.repository.FastenerRepository;
 import bg.mck.ordercommandservice.repository.GalvanisedSheetRepository;
 import bg.mck.ordercommandservice.repository.InsulationRepository;
+import bg.mck.ordercommandservice.repository.MetalRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,7 +38,12 @@ public class UpdateOrderService {
 
     private final InsulationMapper insulationMapper;
 
-    public UpdateOrderService(FastenerRepository fastenerRepository, OrderQueryServiceClient orderQueryServiceClient, FastenerMapper fastenerMapper, GalvanisedSheetRepository galvanisedSheetRepository, GalvanisedSheetMapper galvanisedSheetMapper, InsulationRepository insulationRepository, InsulationMapper insulationMapper) {
+    private final MetalRepository metalRepository;
+
+    private final MetalMapper metalMapper;
+
+
+    public UpdateOrderService(FastenerRepository fastenerRepository, OrderQueryServiceClient orderQueryServiceClient, FastenerMapper fastenerMapper, GalvanisedSheetRepository galvanisedSheetRepository, GalvanisedSheetMapper galvanisedSheetMapper, InsulationRepository insulationRepository, InsulationMapper insulationMapper, MetalRepository metalRepository, MetalMapper metalMapper) {
         this.fastenerRepository = fastenerRepository;
         this.orderQueryServiceClient = orderQueryServiceClient;
         this.fastenerMapper = fastenerMapper;
@@ -43,6 +51,8 @@ public class UpdateOrderService {
         this.galvanisedSheetMapper = galvanisedSheetMapper;
         this.insulationRepository = insulationRepository;
         this.insulationMapper = insulationMapper;
+        this.metalRepository = metalRepository;
+        this.metalMapper = metalMapper;
     }
 
     @Transactional
@@ -55,7 +65,9 @@ public class UpdateOrderService {
             case INSULATION:
                 updateInsulationEntity(updateOrderDTO);
             case METAL:
+                updateMetalEntity(updateOrderDTO);
             case PANELS:
+//                updatePanelsEntity(updateOrderDTO);
             case REBAR:
             case UNSPECIFIED:
             case SERVICE:
@@ -63,10 +75,20 @@ public class UpdateOrderService {
         }
     }
 
+    private void updateMetalEntity(UpdateOrderDTO updateOrderDTO) {
+        Optional<MetalEntity> metalEntity = this.metalRepository
+                .findById(Long.parseLong(updateOrderDTO.getId()));
+        metalMapper.toUpdateMetalEntity(updateOrderDTO, metalEntity.get());
+        this.metalRepository.save(metalEntity.get());
+        sendEvent(updateOrderDTO);
+    }
+
     private void updateInsulationEntity(UpdateOrderDTO updateOrderDTO) {
         Optional<InsulationEntity> insulationEntity = this.insulationRepository
                 .findById(Long.parseLong(updateOrderDTO.getId()));
         insulationMapper.toUpdateInsulationEntity(updateOrderDTO, insulationEntity.get());
+        this.insulationRepository.save(insulationEntity.get());
+        sendEvent(updateOrderDTO);
     }
 
     private void updateFastenerEntity(UpdateOrderDTO updateOrderDTO) {
