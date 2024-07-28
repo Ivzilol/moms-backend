86b3890cec29f8b63c171add17372ef854248125
Borovaneca <petyoveselinov@gmail.com>
2024-07-29 00:41:45 +0300

    Implementing swagger on ConstructionSiteController

--- order-query-service/src/main/java/bg/mck/orderqueryservice/controller/ConstructionSiteController.java
+++ order-query-service/src/main/java/bg/mck/orderqueryservice/controller/ConstructionSiteController.java
@@ -2,6 +2,10 @@ package bg.mck.orderqueryservice.controller;
 
 import bg.mck.orderqueryservice.dto.ConstructionSiteDTO;
 import bg.mck.orderqueryservice.service.ConstructionSiteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,16 +24,30 @@ public class ConstructionSiteController {
         this.constructionSiteService = constructionSiteService;
     }
 
+    @Operation(summary = "Get a construction site by its name", description = "Fetches a construction site using its name.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved construction site"),
+            @ApiResponse(responseCode = "404", description = "Construction site not found")
+    })
     @GetMapping("/{constructionSiteName}")
-    public ResponseEntity<ConstructionSiteDTO> getConstructionSiteByName(@PathVariable String constructionSiteName) {
+    public ResponseEntity<ConstructionSiteDTO> getConstructionSiteByName(
+            @Parameter(description = "Name of the construction site") @PathVariable String constructionSiteName) {
         return ResponseEntity.ok(constructionSiteService.getConstructionSiteByName(constructionSiteName));
     }
 
+    @Operation(summary = "Get a construction site by its ID", description = "Fetches a construction site using its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved construction site"),
+            @ApiResponse(responseCode = "404", description = "Construction site not found")
+    })
     @GetMapping("/{constructionSiteId}")
-    public ResponseEntity<ConstructionSiteDTO> getConstructionSiteById(@PathVariable String constructionSiteId) {
+    public ResponseEntity<ConstructionSiteDTO> getConstructionSiteById(
+            @Parameter(description = "ID of the construction site") @PathVariable String constructionSiteId) {
         return ResponseEntity.ok(constructionSiteService.getConstructionSiteById(constructionSiteId));
     }
 
+    @Operation(summary = "Get all construction sites", description = "Fetches a list of all construction sites.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of construction sites")
     @GetMapping("/all")
     public ResponseEntity<List<ConstructionSiteDTO>> getAllConstructionSites() {
         return ResponseEntity.ok(constructionSiteService.getAllConstructionSites());
