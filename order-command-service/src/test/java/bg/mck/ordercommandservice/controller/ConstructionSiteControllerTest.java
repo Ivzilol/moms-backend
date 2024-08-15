9dec8167247d10f534d28f981e4694eccd4adfad
Mihail Slavov <mihail_slavov@icloud.com>
2024-08-15 14:41:56 +0300

    fix the tests for create construction site and added for the update

--- order-command-service/src/test/java/bg/mck/ordercommandservice/controller/ConstructionSiteControllerTest.java
+++ order-command-service/src/test/java/bg/mck/ordercommandservice/controller/ConstructionSiteControllerTest.java
@@ -18,9 +18,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,6 +42,7 @@ class ConstructionSiteControllerTest {
 
     @MockBean
     private OrderQueryServiceClient orderQueryServiceClient;
+
     @Autowired
     private ConstructionSiteRepository constructionSiteRepository;
 
@@ -67,7 +71,7 @@ class ConstructionSiteControllerTest {
     }
 
     @Test
-    void testCreateConstructionSiteAlreadyExists() throws Exception {
+    void testCreateConstructionSite_AlreadyExistsNameAndNumber() throws Exception {
 
         ConstructionSiteEntity constructionSiteEntity = new ConstructionSiteEntity();
         constructionSiteEntity.setConstructionNumber("12345");
@@ -81,8 +85,19 @@ class ConstructionSiteControllerTest {
     }
 
     @Test
-    void testCreateConstructionSite_Success() throws Exception {
+    void testCreateConstructionSite_MissingName() throws Exception {
 
+        constructionSiteDTO.setName(null);
+
+        mockMvc.perform(post("/V1/admin/order/command/create-construction-site")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(constructionSiteDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("name").value("Construction site name must not be empty."));
+    }
+
+    @Test
+    void testCreateConstructionSite_SuccessEventCreation() throws Exception {
         // When
         mockMvc.perform(post("/V1/admin/order/command/create-construction-site")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -103,6 +118,62 @@ class ConstructionSiteControllerTest {
         assertThat(eventCaptor.getValue().getEvent().getId()).isNotNull();
         assertThat(eventCaptor.getValue().getEvent().getName()).isEqualTo("New Site");
         assertThat(eventCaptor.getValue().getEvent().getConstructionNumber()).isEqualTo("12345");
+    }
+
+    @Test
+    public void testUpdateConstructionSite() throws Exception {
+        // Given
+        ConstructionSiteEntity constructionSiteEntity = new ConstructionSiteEntity();
+        constructionSiteEntity.setConstructionNumber("123123");
+        constructionSiteEntity.setName("New Site123");
+        constructionSiteRepository.save(constructionSiteEntity);
+
+        ConstructionSiteDTO constructionSiteDTO = new ConstructionSiteDTO();
+        constructionSiteDTO.setConstructionNumber("18.23.1");
+        constructionSiteDTO.setName("New Updated Site");
+        constructionSiteDTO.setId(1L);
+
+        // When
+        mockMvc.perform(patch("/V1/admin/order/command/update-construction-site")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(constructionSiteDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.constructionNumber").value("18.23.1"))
+                .andExpect(jsonPath("$.name").value("New Updated Site"));
+
+        // Then
+        ArgumentCaptor<EventData<ConstructionSiteEvent>> eventCaptor = ArgumentCaptor.forClass(EventData.class);
+        ArgumentCaptor<String> eventTypeCaptor = ArgumentCaptor.forClass(String.class);
+
+        // Verify that the sendConstructionSiteEvent method was called
+        verify(orderQueryServiceClient, times(1)).sendConstructionSiteEvent(eventCaptor.capture(), eventTypeCaptor.capture());
+
+        // Optionally, you can add more assertions to check the content of the captured arguments
+        assertThat(eventTypeCaptor.getValue()).isEqualTo("CONSTRUCTION_SITE_UPDATED");
+        assertThat(eventCaptor.getValue().getEvent().getId()).isNotNull();
+        assertThat(eventCaptor.getValue().getEvent().getName()).isEqualTo("New Updated Site");
+        assertThat(eventCaptor.getValue().getEvent().getConstructionNumber()).isEqualTo("18.23.1");
+
+
+        // Then
+        assertThat(constructionSiteRepository.findAll()).hasSize(1);
+        assertThat(constructionSiteRepository.findAll().get(0).getId()).isEqualTo(1L);
+        assertThat(constructionSiteRepository.findAll().get(0).getName()).isEqualTo("New Updated Site");
+        assertThat(constructionSiteRepository.findAll().get(0).getConstructionNumber()).isEqualTo("18.23.1");
+    }
+
+    @Test
+    void testUpdateConstructionSite_AlreadyExists() throws Exception {
+
+        ConstructionSiteEntity constructionSiteEntity = new ConstructionSiteEntity();
+        constructionSiteEntity.setConstructionNumber("12345");
+        constructionSiteEntity.setName("New Site");
+        constructionSiteRepository.save(constructionSiteEntity);
+
+        mockMvc.perform(patch("/V1/admin/order/command/update-construction-site")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(constructionSiteDTO)))
+                .andExpect(status().isConflict());
     }
 
 
