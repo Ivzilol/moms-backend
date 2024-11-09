72375c53ca5f51a3b55e61e636b3c18a596de463
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-09 18:18:37 +0200

    refactor

--- user-query-service/src/main/java/bg/mck/userqueryservice/application/events/ProfileStatusUpdatedEvent.java
+++ user-query-service/src/main/java/bg/mck/userqueryservice/application/events/ProfileStatusUpdatedEvent.java
@@ -1,20 +1,11 @@
 package bg.mck.userqueryservice.application.events;
 
 
-import bg.mck.userqueryservice.application.enums.EventType;
-
-import java.time.LocalDateTime;
-
 public class ProfileStatusUpdatedEvent extends BaseEvent {
 
     private boolean isActive;
 
     public ProfileStatusUpdatedEvent() {
-    }
-
-    public ProfileStatusUpdatedEvent(EventType eventType, Long userId, LocalDateTime localDateTime, boolean isActive) {
-        super(eventType, userId, localDateTime);
-        this.isActive = isActive;
     }
 
     public boolean isActive() {
