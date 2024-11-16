82b559b1416cc419928e247f3bfe49770e37655a
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-16 13:03:32 +0200

    refactor

--- user-command-service/src/main/java/bg/mck/usercommandservice/application/exceptions/InvalidPasswordException.java
+++ user-command-service/src/main/java/bg/mck/usercommandservice/application/exceptions/InvalidPasswordException.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid password.")
 public class InvalidPasswordException extends RuntimeException{
-
     public InvalidPasswordException(String message) {
         super(message);
     }
