d20fe42b250227d3537059dc67f987f1f51acf0e
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-16 13:01:53 +0200

    refactor

--- user-command-service/src/main/java/bg/mck/usercommandservice/application/exceptions/ErrorUserRegistrationExceptions.java
+++ user-command-service/src/main/java/bg/mck/usercommandservice/application/exceptions/ErrorUserRegistrationExceptions.java
@@ -5,7 +5,6 @@ public class ErrorUserRegistrationExceptions {
     public static final String EMPTY_PASSWORD = "Password cannot be empty";
     public static final String EMPTY_CONFIRM_PASSWORD = "Confirm password cannot be empty";
     public static final String EMPTY_FIRST_NAME = "First Name cannot be empty";
-
     public static final String EMPTY_LAST_NAME = "Last Name password cannot be empty";
 
     public static final String EMPTY_PHONE_NUMBER = "Phone Number cannot be empty";
