78d300d8e1b258f258d58392f391488dbfd71965
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-17 18:33:12 +0200

    refactor

--- user-command-service/src/main/java/bg/mck/usercommandservice/application/repository/AuthorityRepository.java
+++ user-command-service/src/main/java/bg/mck/usercommandservice/application/repository/AuthorityRepository.java
@@ -3,7 +3,9 @@ package bg.mck.usercommandservice.application.repository;
 import bg.mck.usercommandservice.application.entity.Authority;
 import bg.mck.usercommandservice.application.enums.AuthorityEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
     Authority getAuthorityByAuthority(AuthorityEnum authorityEnum);
 }
