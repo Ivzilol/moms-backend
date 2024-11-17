116ff9c48eb4c9f38437a6df12a01422bf1c2575
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-17 18:32:47 +0200

    refactor

--- user-command-service/src/main/java/bg/mck/usercommandservice/application/mapper/UserMapper.java
+++ user-command-service/src/main/java/bg/mck/usercommandservice/application/mapper/UserMapper.java
@@ -6,7 +6,6 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
     UserEntity toEntity(UserUpdatedDTO userUpdatedDTO);
     UserUpdatedDTO toDTO(UserEntity userEntity);
 }
