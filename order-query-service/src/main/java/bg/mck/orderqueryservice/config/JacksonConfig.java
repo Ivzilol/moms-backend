4c4f57b4cc066ba8e95a22cad1e5b1cf483f3b66
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-11-30 10:57:25 +0200

    refactor

--- order-query-service/src/main/java/bg/mck/orderqueryservice/config/JacksonConfig.java
+++ order-query-service/src/main/java/bg/mck/orderqueryservice/config/JacksonConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
